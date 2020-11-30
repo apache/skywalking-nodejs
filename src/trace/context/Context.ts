9301d3620bf526a926ec326d217d4c4b9d29ab2b
Tomasz Pytel <tompytel@gmail.com>
2020-11-29 22:07:38 -0300

    documented .async() and .resync()

--- src/trace/context/Context.ts
+++ src/trace/context/Context.ts
@@ -35,8 +35,13 @@ export default interface Context {
 
   stop(span: Span): boolean;
 
+  /* This should be called just before a span is passed to a different async context, like for example a callback from
+     an asynchronous operation the code belonging to the span initiated. After this is called a span should only call
+     .resync() or .stop(). See HttpPlugin.interceptClientRequest() in plugins/HttpPlugin.ts for example of usage. */
   async(span: Span): void;
 
+  /* This should be called upon entering the new async context for a span that has previously executed .async(), it
+     should be the first thing the callback function belonging to the span does. */
   resync(span: Span): void;
 
   currentSpan(): Span | undefined;
