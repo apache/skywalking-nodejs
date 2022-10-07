f838ffc762807fdab4eed74865d77ac8e711ca57
Michael Zangl <git@michaelzangl.de>
2022-10-07 12:33:19 +0200

    Ignore no requests if ignoreSuffix is empty (#94)
    An empty ignore suffix currently causes all requests to be ignored. This makes it impossible to disable ignoring by suffix.

--- src/config/AgentConfig.ts
+++ src/config/AgentConfig.ts
@@ -53,10 +53,19 @@ export function finalizeConfig(config: AgentConfig): void {
     'i',
   );
 
-  const ignoreSuffix = `^.+(?:${config
-    .ignoreSuffix!.split(',')
-    .map((s) => escapeRegExp(s.trim()))
-    .join('|')})$`;
+  const convertIgnoreSuffix = (configuredIgnoreSuffix: string | undefined) => {
+    if (!configuredIgnoreSuffix) {
+      // This regexp will never match => no files are ignored.
+      return '\\A(?!x)x';
+    } else {
+      return `^.+(?:${configuredIgnoreSuffix!
+        .split(',')
+        .map((s) => escapeRegExp(s.trim()))
+        .join('|')})$`;
+    }
+  };
+
+  const ignoreSuffix = convertIgnoreSuffix(config.ignoreSuffix);
   const ignorePath =
     '^(?:' +
     config
